2e23153b9bd8fe39a68681733072a4607502c031
Nityam <nityam@trykapi.com>
2023-08-01 19:29:58 +0530

    feat: unused code removal -5

---
+++ product/apps/web/app/product/page.tsx
@@ -0,0 +1,55 @@
+import React from "react";
+
+import ProductDescription from "../components/ProductDescription";
+import ImageCarousel from "../components/ImageCarousel";
+import { Loader } from "ui";
+import loadable from "next/dynamic";
+import "../../styles/main.css";
+
+export const dynamic = "force-dynamic";
+
+const ProductCarousel = loadable(
+  () => import("../components/ProductCarousel"),
+  {
+    loading: () => <Loader />,
+    ssr: false,
+  }
+);
+
+interface Product {
+  id: string;
+  name: string;
+  description: string;
+  price: number;
+  imageUrl: string;
+}
+
+interface ProductDetailProps {
+  product: Product;
+}
+
+export default async function Page() {
+  const data = await fetch(`http://0.0.0.0:3000/api/product/getById/1234`, {
+    cache: "no-cache",
+  });
+  const result = await data.json();
+  const {
+    product,
+    similar_product: { products },
+  } = result;
+  if (!result) {
+    <></>;
+  }
+  return (
+    <>
+      <div className="component-product">
+        <div className="component-images-container">
+          <ImageCarousel images={product.images}></ImageCarousel>
+          <ProductDescription product={product} />
+        </div>
+        <h2>Similar Products</h2>
+        <ProductCarousel products={products} />
+      </div>
+    </>
+  );
+}
